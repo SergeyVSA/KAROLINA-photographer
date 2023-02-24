488f16a3981adc747cb6a1a77d0d01563ef34241
SergeyVSA <vsamsk77@gmail.com>
2023-02-24 18:11:58 +0300

    responsive

--- script.js
+++ script.js
@@ -25,3 +25,33 @@ $(function () {
     })
     
 });
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+window.addEventListener("DOMContentLoaded", function () {
+    document.querySelector(".mobile_menu_btn").addEventListener("click", function () {
+        document.querySelector(".mobile_menu").classList.toggle("hidden");
+        
+
+    });
+    document.body.addEventListener("click", function (e) {
+        if (!e.target.closest(".mobile_menu_btn")) {
+            document.querySelector(".mobile_menu").classList.add("hidden");
+            
+        }
+    });
+   
+});
